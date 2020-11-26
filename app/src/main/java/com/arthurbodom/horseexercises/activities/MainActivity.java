b16994d71210af23542e43b82f28ee91ee80db6f
ArthurBodom <arthur.lehirreynaud@gmail.com>
2020-11-26 09:49:38 +0100

    Exercise display test

--- app/src/main/java/com/arthurbodom/horseexercises/activities/MainActivity.java
+++ app/src/main/java/com/arthurbodom/horseexercises/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.arthurbodom.horseexercises.R;
+import com.arthurbodom.horseexercises.models.Exercise;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv_test = findViewById(R.id.tv_test);
+        Exercise avancer = new Exercise(1, "Avancer", "Travail à pied", "1 - Facile", "res", "Faire avancer son cheval", "Faire en sorte que son cheval se déplace vers l'avant", "Vérifier qu'il va bien vers l'avant et non vers l'arrière ", "Pas");
 
-        String chibrax = "Chibre";
+        TextView tv_name = findViewById(R.id.tv_name);
+        TextView tv_category = findViewById(R.id.tv_category);
+        TextView tv_level = findViewById(R.id.tv_level);
+        TextView tv_description = findViewById(R.id.tv_description);
+        TextView tv_objectives = findViewById(R.id.tv_objectives);
 
-        tv_test.setText(chibrax);
+        tv_name.setText(avancer.getName());
+        tv_category.setText(avancer.getCategory());
+        tv_level.setText(avancer.getLevel());
+        tv_description.setText(avancer.getDescription());
+        tv_objectives.setText(avancer.getObjectives());
     }
 }
